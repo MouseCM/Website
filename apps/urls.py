549bcb4dfce460dcb421f2128220d299bcb4ab60
Mouse <Mousecuber@gmail.com>
2023-09-25 11:23:19 +0700

    modified:   apps/urls.py 	modified:   apps/views.py

--- apps/urls.py
+++ apps/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('contact/', views.contact)
 ]
--- apps/views.py
+++ apps/views.py
@@ -3,6 +3,3 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html')
-
-def contact(request):
-    return render(request, 'contact.html')
