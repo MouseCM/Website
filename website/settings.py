a62aa658a89e9610d018f21616c2939cce7ee599
Mouse <Mousecuber@gmail.com>
2023-09-25 08:36:46 +0700

    modified:   manage.py 	modified:   website/asgi.py 	modified:   website/manage.py 	modified:   website/settings.py 	modified:   website/wsgi.py

--- website/settings.py
+++ website/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'mouse.urls'
+ROOT_URLCONF = 'website.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mouse.wsgi.application'
+WSGI_APPLICATION = 'website.wsgi.application'
 
 
 # Database
